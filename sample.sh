e2d8e5fa42cf1d3622ad1182065b7d642ea0c63b
hjilakara <78323141+hjilakara@users.noreply.github.com>
2021-02-13 16:35:18 -0500

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -3,10 +3,7 @@
 echo 'Hello World'
 echo 'Modified the proram'
 echo 'added a new line in dev'
-<<<<<<< HEAD
 echo 'added a second line in dev'
-=======
 ls -lrt;
 id;
 pwd;
->>>>>>> 9bbb9f8893b1d3a8d4376757edb4682b6a8db0c8
