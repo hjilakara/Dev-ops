a241b85806d983e9d733e1b783fbc3b36abe629e
hjilakara <78323141+hjilakara@users.noreply.github.com>
2021-02-13 16:41:11 -0500

    Dev (#2)
    * Update sample.sh
    
    * Without latest clone
    
    * Update sample.sh
    
    * Update sample.sh
    
    Co-authored-by: Hanumesh Jilakara <hjilakar@in.ibm.com>

--- sample.sh
+++ sample.sh
@@ -2,4 +2,7 @@
 # Sample shell scritp to push to GITHUB server
 echo 'Hello World'
 echo 'Modified the proram'
-echo 'added a new line in dev'
+echo 'added a second line in dev'
+ls -lrt;
+id;
+pwd;
